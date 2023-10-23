70bbf03b4b1bea5a20bf853fa6a4ea79d9f6caf2
akaboshinit <akaboshinit@gmail.com>
2023-10-24 02:49:55 +0900

    initial commit

---
+++ flexible_stickies/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  flexible_stickies
+//
+//  Created by redstar16 on 2023/10/23.
+//
+
+import Foundation
---
+++ flexible_stickies/flexible_stickies/flexible_stickiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  flexible_stickiesApp.swift
+//  flexible_stickies
+//
+//  Created by redstar16 on 2023/10/23.
+//
+
+import SwiftUI
+
+@main
+struct flexible_stickiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
